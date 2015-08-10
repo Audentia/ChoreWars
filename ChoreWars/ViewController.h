601005b5e783a844c52d1a18109f95cc31134a7c
Douglas Hewitt <doughewitt4@gmail.com>
2015-08-09 22:26:57 -0700

    Initial Project made in Xcode

---
+++ ChoreWars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChoreWars
+//
+//  Created by Douglas Hewitt on 8/9/15.
+//  Copyright © 2015 madebydouglas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChoreWarsTests/ChoreWarsTests.m
@@ -0,0 +1,39 @@
+//
+//  ChoreWarsTests.m
+//  ChoreWarsTests
+//
+//  Created by Douglas Hewitt on 8/9/15.
+//  Copyright © 2015 madebydouglas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChoreWarsTests : XCTestCase
+
+@end
+
+@implementation ChoreWarsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
