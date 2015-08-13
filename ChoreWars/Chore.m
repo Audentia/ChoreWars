3ee5a294c6a56560487326b8b0618abba028ed0d
Douglas Hewitt <doughewitt4@gmail.com>
2015-08-13 13:58:44 -0700

    replaced Core Data nsManaged objects
    now it works again

--- ChoreWars/Chore.m
+++ ChoreWars/Chore.m
@@ -2,7 +2,7 @@
 //  Chore.m
 //  ChoreWars
 //
-//  Created by Douglas Hewitt on 8/10/15.
+//  Created by Douglas Hewitt on 8/13/15.
 //  Copyright © 2015 madebydouglas. All rights reserved.
 //
 
--- ChoreWars/Roommate.m
+++ ChoreWars/Roommate.m
@@ -2,11 +2,13 @@
 //  Roommate.m
 //  ChoreWars
 //
-//  Created by Douglas Hewitt on 8/10/15.
+//  Created by Douglas Hewitt on 8/13/15.
 //  Copyright © 2015 madebydouglas. All rights reserved.
 //
 
 #import "Roommate.h"
+#import "Chore.h"
+#import "Team.h"
 
 @implementation Roommate
 
